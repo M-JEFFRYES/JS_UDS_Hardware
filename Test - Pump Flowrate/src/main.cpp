bdb9aa5f529aca8a8f974547a06df1ba84777844
UrodynamicsWB <urodynamicswithoutborders@gmail.com>
2023-05-24 23:15:32 +0100

    Working

--- Test - Pump Flowrate/src/main.cpp
+++ Test - Pump Flowrate/src/main.cpp
@@ -1,18 +1,91 @@
 #include <Arduino.h>
+#include <HX711.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define calibration_factor 1 //This value is obtained using the SparkFun_HX711_Calibration sketch
+#define VI_DOUT  4
+#define VI_SCK  5
+
+const int BAUD_RATE = 9600;
+
+const int START_TRANSMISSION_CODE = 333;
+const int END_TRANSMISSION_CODE = 444;
+const int PUMP_PWM_MAX = 256;
+const unsigned int MAX_MESSAGE_LENGTH = 5;
+
+float millis_start;
+bool send_data;
+int pump_value;
+float time;
+float vi;
+
+HX711 loadcell;
+
+void initValues();
+void updateValues();
+void printValues();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(BAUD_RATE);
+  initValues();
+
+  loadcell.begin(VI_DOUT, VI_SCK);
+  loadcell.set_scale(calibration_factor); 
+  //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  //scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (Serial.available()>0){
+    static char message[MAX_MESSAGE_LENGTH];
+    static unsigned int message_pos=0;
+
+    char inByte = Serial.read();
+
+    if (inByte != '\n'){
+      message[message_pos] = inByte;
+      message_pos++;
+
+    } else {
+      message[message_pos] = '\0';
+      int number = atoi(message);
+
+      if (number == START_TRANSMISSION_CODE){
+        millis_start = millis();
+        vi = loadcell.read();
+        send_data = true;
+      } else if (number == END_TRANSMISSION_CODE) {
+        send_data = false;
+      } else if (number < PUMP_PWM_MAX ) {
+        pump_value = number;
+      }
+
+      message_pos = 0;
+    }
+  }
+
+  if (send_data){
+    updateValues();
+    printValues();
+  }
+  delay(2);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void initValues(){
+  millis_start = 0;
+  send_data = false;
+  time = 0.0;
+  vi = 0.0;
 }
+
+void updateValues() {
+  time = millis() - millis_start;
+  vi = loadcell.read();
+}
+
+void printValues() {
+  Serial.print("Time,");
+  Serial.print(millis());
+  Serial.print(";VI,");
+  Serial.println(vi);
+}
+
--- Test - Volume Void/src/main.cpp
+++ Test - Volume Void/src/main.cpp
@@ -2,32 +2,87 @@
 #include <HX711.h>
 
 #define calibration_factor 1 //This value is obtained using the SparkFun_HX711_Calibration sketch
-#define VI_DOUT  2
-#define VI_SCK  3
+#define VV_DOUT  2
+#define VV_SCK  3
+
+const int BAUD_RATE = 9600;
+
+const int START_TRANSMISSION_CODE = 333;
+const int END_TRANSMISSION_CODE = 444;
+const unsigned int MAX_MESSAGE_LENGTH = 5;
+
+float millis_start;
+bool send_data;
+
+float time;
+float vv;
 
 HX711 loadcell;
-void readVolumeVoidLoadcell();
+
+void initValues();
+void updateValues();
+void printValues();
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("UDSWB Volume Void System Testing");
-  
-  loadcell.begin(VI_DOUT, VI_SCK);
-  Serial.println("Loadcell initiated");
+  Serial.begin(BAUD_RATE);
+  initValues();
 
+  loadcell.begin(VV_DOUT, VV_SCK);
   loadcell.set_scale(calibration_factor); 
   //This value is obtained by using the SparkFun_HX711_Calibration sketch
   //scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
-  Serial.println("Readings:");
 }
 
 void loop() {
-  readVolumeVoidLoadcell();
-  delay(20);
+  while (Serial.available()>0){
+    static char message[MAX_MESSAGE_LENGTH];
+    static unsigned int message_pos=0;
+
+    char inByte = Serial.read();
+
+    if (inByte != '\n'){
+      message[message_pos] = inByte;
+      message_pos++;
+
+    } else {
+      message[message_pos] = '\0';
+      int number = atoi(message);
+
+      if (number == START_TRANSMISSION_CODE){
+        millis_start = millis();
+        vv = loadcell.read();
+        send_data = true;
+      } else if (number == END_TRANSMISSION_CODE) {
+        send_data = false;
+      } 
+
+      message_pos = 0;
+    }
+  }
+
+  if (send_data){
+    updateValues();
+    printValues();
+  }
+  delay(2);
 }
 
-void readVolumeVoidLoadcell() {
-  Serial.print(millis());
-  Serial.print(",");
-  Serial.println(loadcell.read());
+void initValues(){
+  millis_start = 0;
+  send_data = false;
+  time = 0.0;
+  vv = 0.0;
 }
+
+void updateValues() {
+  time = millis() - millis_start;
+  vv = loadcell.read();
+}
+
+void printValues() {
+  Serial.print("Time,");
+  Serial.print(millis());
+  Serial.print(";VV,");
+  Serial.println(vv);
+}
+
